8b39bf9fe65196baf156f68c8f347ed95115194c
Sam Perozek <samperozek@gmail.com>
2022-03-08 10:43:01 -0700

    commit tree 99b8d46a4f04f2cb2f1da8256561f82fb7cea63a

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a,b){
+    let sum;
+    return sum = a+b;
+}
+function subtract(a,b){
+    let diff;
+    return diff = a-b;
+}
+function multiply(a,b){
+    let total;
+    return total = a*b;
+}
+function divide(a,b){
+    let total;
+    return total = a/b;
+}
+function increment(n){
+    return n+=1;
+}
+function decrement(n){
+    return n-=1;
+}
+function makeInt(n){
+    return parseInt(n,10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
